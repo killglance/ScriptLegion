873fdc24678ea1c068ecd114fc932b5ef7ffbc8d
Jon Schulz <jon.schulz@gmail.com>
2009-11-30 23:42:03 -0500

    Moved config parsing into its own method. Changed host tag to Servers

--- ScriptLegion.rb
+++ ScriptLegion.rb
@@ -2,22 +2,32 @@
 
 require "Host.rb"
 
+def parse_config( my_file )
+   config = {}
+   while my_file.gets do
+      if $_.match /\[(.+?)\]/
+         insert_point = $1
+         config[insert_point] = {}
+      elsif $_.match /(.+?)=(.+)/
+         config[insert_point][$1.strip] = $2.strip
+      end
+   end
+   return config
+end
+      
+
 file = File.open( "hosts.cfg" )
 
-config = {}
-config["globals"] = {}
-insert_point = "globals"
+config = parse_config( file )
+#while file.gets do
+#  if $_.match /\[(.+?)\]/
+#    insert_point = $1
+#    config[insert_point] = {}
+#  elsif $_.match /(.+?)=(.+)/
+#    config[insert_point][$1.strip] = $2.strip
+#  end
+#end
 
-while file.gets do
-  if $_.match /\[(.+?)\]/
-    insert_point = $1
-    config[insert_point] = {}
-  elsif $_.match /(.+?)=(.+)/
-    config[insert_point][$1.strip] = $2.strip
-  end
-end
-
-myHost = Host.new( config["ThisHost"]["hostname"], config["ThisHost"]["ip"] )
-#myHost = Host.new("localhost", "127.0.0.1")
+myHost = Host.new( config["Servers"]["hostname"], config["Servers"]["ip"] )
 
 puts myHost
